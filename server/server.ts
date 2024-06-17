a0446ff94ee19c506890ea629fdc448de17756cb
Boris Gingold <boris.gingold@bogitech.de>
2024-06-17 20:05:53 +0200

    Nur Zahlen/Buchstaben und ein paar Befehle erkennen

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import vosk from 'vosk';
+import * as vosk from 'vosk';
 import * as fs from "node:fs";
 
 
@@ -10,7 +10,7 @@ const MODEL_PATH = "model"
 const FILE_NAME = "test.wav"
 
 if (!fs.existsSync(MODEL_PATH)) {
-    console.log("Please download the model from https://alphacephei.com/vosk/models and unpack as " + MODEL_PATH + " in the current folder.")
+      console.log("Please download the model from https://alphacephei.com/vosk/models and unpack as " + MODEL_PATH + " in the current folder.")
     process.exit()
 }
 
@@ -25,16 +25,25 @@ wfReader.on('format', async ({ audioFormat, sampleRate, channels }) => {
         console.error("Audio file must be WAV format mono PCM.");
         process.exit(1);
     }
-    const rec = new vosk.Recognizer({model: model, sampleRate: sampleRate});
+
+    const grammar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun",
+        "start",
+        "stop", "pause",
+        "hilfe", "unterstützung",
+        "weiter", "egal", "nächstes", //
+        "mao", "brumm", /* auto zu schnell / keine Zeit zum erfassen */
+    ];
+
+    const rec = new vosk.Recognizer({model: model, sampleRate: sampleRate, grammar: grammar});
     rec.setMaxAlternatives(10);
     rec.setWords(true);
     rec.setPartialWords(true);
     for await (const data of wfReadable) {
         const end_of_speech = rec.acceptWaveform(data);
         if (end_of_speech) {
-            console.log(JSON.stringify(rec.result(), null, 4));
+            //console.log(JSON.stringify(rec.result(), null, 4));
         } else {
-            console.log(JSON.stringify(rec.partialResult(), null, 4));
+            //console.log(JSON.stringify(rec.partialResult(), null, 4));
         }
     }
     let result = rec.finalResult(rec);
